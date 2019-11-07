e7bed61c867a0247188c2b07ffe1d9c6c594a886
Masayuki <nitamagoex@yahoo.co.jp>
2019-11-07 21:14:02 +0900

    implemented sample

---
+++ README.txt
@@ -0,0 +1,2 @@
+”wŒiF‚ğ•Ï‚¦‚éƒTƒ“ƒvƒ‹‚ğÀ‘•‚µ‚Ä‚İ‚½
+https://qiita.com/wjtnk/items/3220afa6748995537358
---
+++ content_script.js
@@ -0,0 +1,3 @@
+﻿chrome.runtime.onMessage.addListener(function(msg) {
+  $("body").css("background-color", msg.color);
+});

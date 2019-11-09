f1baf50cd38631b6fb89e368458504e7d6792f7a
Masayuki <nitamagoex@yahoo.co.jp>
2019-11-09 20:41:38 +0900

    Implement for paly feature

--- content_script.js
+++ content_script.js
@@ -1,3 +1,55 @@
 ﻿chrome.runtime.onMessage.addListener(function(msg) {
-  $("body").css("background-color", msg.color);
+  console.log("Reloaded");
+  observer.disconnect();
+  init();
 });
+
+// 動的ページが読み込まれるまで待つ
+window.addEventListener("load", main, false);
+
+function main(e) {
+    const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+    
+    function jsLoaded() {
+        if (document.getElementsByClassName('card-zoom')[0] != null) {
+            clearInterval(jsInitCheckTimer);
+        }
+
+	init();
+    }
+}
+
+function init(){
+  // For play
+  // card-zoomクラスのノード取得
+  target = document.getElementsByClassName('card-zoom')[0];
+  // Mutation Observer設定
+  observer = new MutationObserver(records => {
+    // 変化が発生したときの処理を記述
+
+    // 無限ループが起きないよう、ObserverをOFFにする
+    observer.disconnect();
+
+    // imgノードを挿入
+    src = "https://res.cloudinary.com/hvtn93q21/image/upload/v1573265136/Netrunner/cards/";
+    preview = $(".card-zoom").children(".card-preview");
+    img_clone = preview.children("img").clone();
+    src_name = img_clone.attr("src");
+    if (src_name != null) {
+      png_name = src_name.split("/")[3];
+      console.log(src + png_name);
+      img_clone.attr("src", src + png_name);
+      preview.append(img_clone);
+    }
+  
+    // ObserverをONにする
+    observer.observe(target, {
+      childList: true
+    });
+  });
+  
+  // DOM監視開始
+  observer.observe(target, {
+    childList: true
+  });
+}
--- popup.js
+++ popup.js
@@ -1,15 +1,4 @@
-﻿$("#black").on("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {
-      color: "black"
-    });
-  });
-});
-
-$("#red").on("click", () => {
-  //送れない！！
-  // chrome.runtime.sendMessage({ color: "red" });
-
+﻿$("#reload").on("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {
       color: "red"
